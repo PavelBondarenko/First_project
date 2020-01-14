6ad2d17a9a45e39b7a963e9967eb022784f48a25
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-01-14 19:02:18 +0300

    DZ2

---
+++ DZ2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $money = 2000;
+    $proc = 11.5;
+    $term = 1;
+
+    // for (; $term <= 14; $term++) {
+    //     $money = $money + ($money * ($proc / 12 / 100);
+    // }
+    // echo $money;
+    do {
+        $money = $money + ($money * ($proc / 12 / 100));
+        $term++;
+    } while (
+        $term <= 14
+    );
+    echo $money;
+
+    ?>
+</body>
+
+</html>
